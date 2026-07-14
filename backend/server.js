9782d521b44b53607ec10b0e222a04fa1c68dba8
shrushti-umit <shrushtirukari2008@gmail.com>
2026-07-15 01:12:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.post("/api/chat",(req,res)=>{
+
+    const {prompt}=req.body;
+
+    res.json({
+        reply:"Server received : "+prompt
+    });
+
+});
+
+app.listen(5000,()=>{
+    console.log("Server Running");
+});
