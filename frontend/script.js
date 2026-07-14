4ea6d127351c7505cbafc0e5e916d85cd8a973f6
shrushti-umit <shrushtirukari2008@gmail.com>
2026-07-15 01:08:54 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,16 +1,20 @@
-function predictGrade() {
+async function sendMessage() {
 
-    let attendance = document.getElementById("attendance").value;
-    let studyHours = document.getElementById("study_hours").value;
-    let previousGrade = document.getElementById("previous_grade").value;
+    const userInput = document.getElementById("prompt").value;
 
-    if(attendance==="" || studyHours==="" || previousGrade===""){
-        document.getElementById("result").innerHTML = "Please fill all the fields.";
-        return;
-    }
+    const response = await fetch("http://localhost:5000/api/chat", {
+        method: "POST",
 
-    let prediction = (Number(attendance) + Number(studyHours) + Number(previousGrade)) / 3;
+        headers: {
+            "Content-Type": "application/json"
+        },
 
-    document.getElementById("result").innerHTML =
-    "Predicted Grade: " + prediction.toFixed(2);
+        body: JSON.stringify({
+            prompt: userInput
+        })
+    });
+
+    const data = await response.json();
+
+    document.getElementById("response").innerText = data.reply;
 }
