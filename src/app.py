eafa44f53bd9231f9703f1721a234f0087fa0ae5
shrushti-umit <shrushtirukari2008@gmail.com>
2026-07-15 23:27:18 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+
 app = Flask(__name__)
+
 chat_history = []
 
 model = joblib.load("model.pkl")
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -17,23 +20,26 @@ def predict():
         input_data = pd.DataFrame([data])
 
         prediction = model.predict(input_data)
+
         chat_history.append({
-    "input": data,
-    "prediction": float(prediction[0])
-})
+            "input": data,
+            "prediction": float(prediction[0])
+        })
 
         return jsonify({
-            "prediction": prediction[0]
+            "prediction": float(prediction[0])
         })
 
     except Exception as e:
         return jsonify({
             "error": str(e)
         }), 500
-        @app.route("/history", methods=["GET"])
+
+
+@app.route("/history", methods=["GET"])
 def history():
     return jsonify(chat_history)
 
+
 if __name__ == "__main__":
     app.run(debug=True)
-   
